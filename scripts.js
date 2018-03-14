6228050b8e51272504c7bc1b62a52535ed5a340a
Marcin Ślęzak <zstio.informatyka@gmail.com>
2018-03-14 21:09:22 +0100

    Corrected JS

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
+/*
 var triangle1Area = getTriangleArea(-5, 10),
     triangle2Area = getTriangleArea(2, 3),
-    triangle3Area = getTriangleArea(30, -50);
+    triangle3Area = getTriangleArea(5, 6);
 
 function getTriangleArea(a, h) {
     if (a <= 0 || h <= 0) {
@@ -9,3 +10,25 @@ function getTriangleArea(a, h) {
       	return console.log(a * h / 2);
     }
 }
+*/
+var a = triangle1Area = getTriangleArea(-5, 10);
+var b = triangle1Area = getTriangleArea(2, 3);
+var c = triangle1Area = getTriangleArea(5, 6);
+var triangle1Area = getTriangleArea(a, b);
+var x = 0;
+function getTriangleArea(a, h) {
+
+  if (!isNaN(a) && !isNaN(h)) {
+    	if ((a > 0) && (h > 0))
+    {
+      	console.log((a * h) / 2);
+      	return x = ((a * h) / 2);
+    } else {
+    	console.log('Parameters are incorrect');
+    }
+  }
+  else {
+    	console.log('Parameters are not numbers');
+  }
+}
+
